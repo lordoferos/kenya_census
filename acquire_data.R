c350b47abe1ae57c10bfd4b85a2947ec8763f641
chevvie <chevviesq@gmail.com>
2021-06-12 19:05:23 +0300

    cleaned data

--- acquire_data.R
+++ acquire_data.R
@@ -1,30 +1,69 @@
 # This is the script used to acquire the various datasets that will be used
 # for this project
 
-# Get the Data
-
-# Read in with tidytuesdayR package 
-# Install from CRAN via: install.packages("tidytuesdayR")
-# This loads the readme and all the datasets for the week of interest
-
-# Either ISO-8601 date or year/week works!
-
-# Option to read the data using the TidyTuesdayR package
-
-# tuesdata <- tidytuesdayR::tt_load('2021-01-19')
-# tuesdata <- tidytuesdayR::tt_load(2021, week = 4)
-
-# gender <- tuesdata$gender
-
-# Or read in the data manually
-
+# Get the Data data manually
+library(rKenyaCensus)
 library(tidyverse)
 
-gender <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-01-19/gender.csv')
-crops <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-01-19/crops.csv')
-households <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-01-19/households.csv')
+# Add other useful datasets
+
+# Household size and population
+households <- V1_T2.3
+# Population and gender by county
+pop_county <- V1_T2.2
+# Births last 5 years by county
+births_county <- V4_T2.40
+# Assets
+assets_owned <- V4_T2.36
+# Searched and bought goods online
+goods_online <- V4_T2.34
+
+
+
+# Clean the datasets
+library(janitor)
+library(tools)
+
+# Remove row containing Kenya (sum total)
+households <- households %>% slice(-1) %>% 
+  clean_names()
+
+# Remove row containing total counts for the sexes
+pop_county <- pop_county %>% slice(-1) %>% 
+  clean_names()
+
+# Remove first 3 rows of rural and urban and whole of Kenya
+# Change county names to sentence case
+births_county <- births_county %>% 
+  slice(-c(1:3)) %>% 
+  mutate(County = toTitleCase(tolower(County)))%>% 
+  clean_names()
+
+# Remove first three rows of rural and urban and whole of Kenya
+# Remove subcounties
+# standardize the naming system
+assets_owned <- assets_owned %>% 
+  ungroup(County) %>%
+  filter(AdminArea == "County") %>% 
+  mutate(County = toTitleCase(tolower(County))) %>% 
+  select(-SubCounty) %>% 
+  clean_names()
+
+# Remove first three rows of rural and urban and whole of Kenya
+# Remove subcounties
+# standardize the naming system
+goods_online <- goods_online %>% 
+  ungroup(County) %>%
+  filter(AdminArea == "County") %>% 
+  mutate(County = toTitleCase(tolower(County))) %>% 
+  select(-SubCounty) %>% 
+  clean_names()
+
 
 # Write the datasets into csv files
-write_csv(gender, "data/gender.csv")
-write_csv(crops, "data/crops.csv")
 write_csv(households, "data/households.csv")
+write_csv(pop_county, "data/pop_county.csv")
+write_csv(births_county, "data/births_county.csv")
+write_csv(assets_owned, "data/assets_owned.csv")
+write_csv(goods_online, "data/goods_online.csv")
+
