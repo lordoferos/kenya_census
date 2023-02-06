35a2361d1709930ac2a63dcef75872d4f2468b55
Levi Orero <33201932+lordoferos@users.noreply.github.com>
2023-02-06 16:16:45 +0300

    gh repo clone lordoferos/kenya_census

---
+++ Census Dash/CensusDash.R
@@ -0,0 +1,28 @@
+library(shiny)
+library(ggplot2)
+
+# Load the data file
+data <- read.csv("county_data.csv")
+
+# Define the Shiny app
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("county", "Select a county:", 
+                  choices = unique(data$county_name))
+    ),
+    mainPanel(
+      plotOutput("county_plot")
+    )
+  )
+)
+
+server <- function(input, output) {
+  output$county_plot <- renderPlot({
+    county_data <- data[data$county_name == input$county, ]
+    ggplot(county_data, aes(x = population_variable, y = population_value)) +
+      geom_bar(stat = "identity")
+  })
+}
+
+shinyApp(ui = ui, server = server)
